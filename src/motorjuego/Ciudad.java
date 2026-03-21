95c85f5e7e6b9dc18e09ac1b5c44c115e86e13f6
fernando <hernandezjaramillo1998@gmail.com>
2026-03-21 15:03:42 +0100

    Inicio las clases de motor de juego, motor,ciudad,main

---
+++ src/motorjuego/Ciudad.java
@@ -0,0 +1,5 @@
+package motorjuego;
+
+public class Ciudad {
+
+}
---
+++ src/motorjuego/EcoCity.java
@@ -0,0 +1,10 @@
+package motorjuego;
+
+public class EcoCity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
